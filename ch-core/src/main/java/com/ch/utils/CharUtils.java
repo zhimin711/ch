f2164178e5aff8bbebcf3fa43be39c8b088a3a8f
仙_梦如尘 <zhimin711@sina.com>
2017-04-04 12:51:55 +0800

    common utils 添加 char

---
+++ ch-core/src/main/java/com/ch/utils/CharUtils.java
@@ -0,0 +1,52 @@
+package com.ch.utils;
+
+import java.util.regex.Pattern;
+
+/**
+ * CharUtils
+ * Created by zhimin on 2017/4/4.
+ */
+public class CharUtils {
+
+
+    // 根据Unicode编码完美的判断中文汉字和符号
+    private static boolean isChinese(char c) {
+        Character.UnicodeBlock ub = Character.UnicodeBlock.of(c);
+        return ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS || ub == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS
+                || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_B
+                || ub == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION || ub == Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS
+                || ub == Character.UnicodeBlock.GENERAL_PUNCTUATION;
+    }
+
+    // 完整的判断中文汉字和符号
+    public static boolean isChinese(String strName) {
+        char[] ch = strName.toCharArray();
+        for (char c : ch) {
+            if (isChinese(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 只能判断部分CJK字符（CJK统一汉字）
+    public static boolean isChineseByREG(String str) {
+        if (str == null) {
+            return false;
+        }
+        Pattern pattern = Pattern.compile("[\\u4E00-\\u9FBF]+");
+        return pattern.matcher(str.trim()).find();
+    }
+
+    // 只能判断部分CJK字符（CJK统一汉字）
+    public static boolean isChineseByName(String str) {
+        if (str == null) {
+            return false;
+        }
+        // 大小写不同：\\p 表示包含，\\P 表示不包含
+        // \\p{Cn} 的意思为 Unicode 中未被定义字符的编码，\\P{Cn} 就表示 Unicode中已经被定义字符的编码
+        String reg = "\\p{InCJK Unified Ideographs}&&\\P{Cn}";
+        Pattern pattern = Pattern.compile(reg);
+        return pattern.matcher(str.trim()).find();
+    }
+}
