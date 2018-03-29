fc733d6e2d00cdb3c58deba5e8a32eb8ebbabc07
仙_梦如尘 <zhimin711@sina.com>
2018-03-29 09:00:44 +0800

    调整MD5

--- ch-core/src/main/java/com/ch/utils/EncryptUtils.java
+++ ch-core/src/main/java/com/ch/utils/EncryptUtils.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 /**
@@ -92,5 +93,31 @@ public class EncryptUtils {
             logger.error("Config tool genKeyPair error!", e);
         }
         return null;
+    }
+
+    public static String md5(String str){
+        return MD5(str);
+    }
+
+    private static String MD5(String s) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] bytes = md.digest(s.getBytes("utf-8"));
+            return toHex(bytes);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String toHex(byte[] bytes) {
+
+        final char[] HEX_DIGITS = "0123456789ABCDEF".toCharArray();
+        StringBuilder ret = new StringBuilder(bytes.length * 2);
+        for (int i=0; i<bytes.length; i++) {
+            ret.append(HEX_DIGITS[(bytes[i] >> 4) & 0x0f]);
+            ret.append(HEX_DIGITS[bytes[i] & 0x0f]);
+        }
+        return ret.toString();
     }
 }
