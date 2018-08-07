7c245000522bea4548d180b728a7d197dd84ed7e
仙_梦如尘 <zhimin711@sina.com>
2018-08-07 11:13:05 +0800

    调整DateUtils

--- ch-core/src/main/java/com/ch/utils/DateUtils.java
+++ ch-core/src/main/java/com/ch/utils/DateUtils.java
@@ -552,6 +552,7 @@ public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
         c.set(Calendar.HOUR_OF_DAY, 0);
         c.set(Calendar.MINUTE, 0);
         c.set(Calendar.SECOND, 0);
+        c.set(Calendar.MILLISECOND, 0);
         return c.getTime();
     }
 
@@ -568,6 +569,7 @@ public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
         c.set(Calendar.HOUR_OF_DAY, 23);
         c.set(Calendar.MINUTE, 59);
         c.set(Calendar.SECOND, 59);
+        c.set(Calendar.MILLISECOND, 999);
         return c.getTime();
     }
 
