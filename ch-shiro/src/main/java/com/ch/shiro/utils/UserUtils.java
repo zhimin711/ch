f23b04fd580f94d8eb1cb6f0fe1d06caa949fad5
仙_梦如尘 <zhimin711@sina.com>
2018-08-11 23:09:31 +0800

    调整Shiro

--- ch-shiro/src/main/java/com/ch/shiro/utils/UserUtils.java
+++ ch-shiro/src/main/java/com/ch/shiro/utils/UserUtils.java
@@ -1,5 +1,7 @@
 package com.ch.shiro.utils;
 
+import com.ch.shiro.authc.Principal;
+import com.ch.utils.CommonUtils;
 import com.ch.utils.StringUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
@@ -10,6 +12,21 @@ import org.apache.shiro.subject.Subject;
  */
 public class UserUtils {
 
+    public static final String SUPER_ADMIN = "SUPER_ADMIN";
+
+    private UserUtils() {
+    }
+
+    public static String getUsername(Object principal) {
+        if (CommonUtils.isEmpty(principal)) return null;
+        if (principal instanceof String) {
+            return (String) principal;
+        } else if (principal instanceof Principal) {
+            return ((Principal) principal).getUsername();
+        }
+        return null;
+    }
+
     /**
      * 获取当前登录用户名
      *
@@ -17,9 +34,9 @@ public class UserUtils {
      */
     public static String getCurrentUsername() {
         try {
-            Subject subject = SecurityUtils.getSubject();
+            Subject subject = getSubject();
             if (subject.isAuthenticated()) {
-                return (String) subject.getPrincipal();
+                return getUsername(subject.getPrincipal());
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -36,5 +53,13 @@ public class UserUtils {
     public static String getCurrentUsername(String defaultUsername) {
         String username = getCurrentUsername();
         return StringUtils.isNotBlank(username) ? username : defaultUsername;
+    }
+
+    public static boolean isSuperAdmin() {
+        return getSubject().hasRole(SUPER_ADMIN);
+    }
+
+    private static Subject getSubject() {
+        return SecurityUtils.getSubject();
     }
 }
