60732033270028139cf6df735232b6ff0bcb29ee
仙_梦如尘 <zhimin711@sina.com>
2018-08-22 17:02:03 +0800

    调整op type

--- ch-core/src/main/java/com/ch/type/OPType.java
+++ ch-core/src/main/java/com/ch/type/OPType.java
@@ -4,6 +4,6 @@ package com.ch.type;
  * @author 01370603
  * @date 2018/8/22 16:52
  */
-public enum OpType {
+public enum OPType {
     START, BEGIN, STOP, END, RESTART, CREATE, ADD, UPDATE, MODIFY, EDIT, DELETE, REMOVE
 }
