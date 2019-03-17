995a8db4e7d13a0892a583fb2de803517c7150be
仙_梦如尘 <zhimin711@sina.com>
2019-03-17 20:29:19 +0800

    调整file utils

--- ch-core/src/main/java/com/ch/utils/FileUtils.java
+++ ch-core/src/main/java/com/ch/utils/FileUtils.java
@@ -43,6 +43,7 @@ public class FileUtils {
      * The number of bytes in an exabyte.
      */
     public static final long ONE_EB = ONE_KB * ONE_PB;
+
     /**
      * 通过文件名获得扩展名(带点)
      *
@@ -79,39 +80,77 @@ public class FileUtils {
     }
 
     /**
-     * Txt file get charset
+     * <pre>
+     * 获取文件的编码
+     * @param file
+     *            File对象实例
+     * </pre>
      *
-     * @param file 文件
-     * @return 编码
+     * @return 文件编码，eg：UTF-8,GBK,GB2312形式(不确定的时候，返回可能的字符编码序列)；若无，则返回null
+     * @throws FileNotFoundException
+     * @throws IOException
      */
+    //判断编码格式方法
     public static String getCharset(File file) {
-        int p = 0;
-        InputStream is = null;
-        BufferedInputStream bin = null;
+        String charset = "GBK";
+        byte[] first3Bytes = new byte[3];
         try {
-            is = new FileInputStream(file);
-            bin = new BufferedInputStream(is);
-            p = (bin.read() << 8) + bin.read();
-        } catch (IOException e) {
+            boolean checked = false;
+            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+            bis.mark(0);
+            int read = bis.read(first3Bytes, 0, 3);
+            if (read == -1) {
+                return charset; //文件编码为 ANSI
+            } else if (first3Bytes[0] == (byte) 0xFF
+                    && first3Bytes[1] == (byte) 0xFE) {
+                charset = "UTF-16LE"; //文件编码为 Unicode
+                checked = true;
+            } else if (first3Bytes[0] == (byte) 0xFE
+                    && first3Bytes[1] == (byte) 0xFF) {
+                charset = "UTF-16BE"; //文件编码为 Unicode big endian
+                checked = true;
+            } else if (first3Bytes[0] == (byte) 0xEF
+                    && first3Bytes[1] == (byte) 0xBB
+                    && first3Bytes[2] == (byte) 0xBF) {
+                charset = "UTF-8"; //文件编码为 UTF-8
+                checked = true;
+            }
+            bis.reset();
+            if (!checked) {
+                int loc = 0;
+                while ((read = bis.read()) != -1) {
+                    loc++;
+                    if (read >= 0xF0)
+                        break;
+                    if (0x80 <= read && read <= 0xBF) // 单独出现BF以下的，也算是GBK
+                        break;
+                    if (0xC0 <= read && read <= 0xDF) {
+                        read = bis.read();
+                        if (0x80 <= read && read <= 0xBF) { // 双字节 (0xC0 - 0xDF)
+                            // (0x80
+                            // - 0xBF),也可能在GB编码内
+                        } else
+                            break;
+                    } else if (0xE0 <= read && read <= 0xEF) {// 也有可能出错，但是几率较小
+                        read = bis.read();
+                        if (0x80 <= read && read <= 0xBF) {
+                            read = bis.read();
+                            if (0x80 <= read && read <= 0xBF) {
+                                charset = "UTF-8";
+                                break;
+                            } else
+                                break;
+                        } else
+                            break;
+                    }
+                }
+            }
+            bis.close();
+        } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            IOUtils.close(bin, is);
         }
-        String code;
-        switch (p) {
-            case 0xefbb:
-                code = "UTF-8";
-                break;
-            case 0xfffe:
-                code = "Unicode";
-                break;
-            case 0xfeff:
-                code = "UTF-16BE";
-                break;
-            default:
-                code = "GBK";
-        }
-        return code;
+        return charset;
+
     }
 
     /**
