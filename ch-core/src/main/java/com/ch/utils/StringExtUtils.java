9806ac1f413df568840f02939cf47048eb8273bb
仙_梦如尘 <zhimin711@sina.com>
2019-05-01 17:53:43 +0800

    string utils 调整

--- ch-core/src/main/java/com/ch/utils/StringExtUtils.java
+++ ch-core/src/main/java/com/ch/utils/StringExtUtils.java
@@ -1,5 +1,7 @@
 package com.ch.utils;
 
+import com.ch.Constants;
+
 /**
  * 描述：com.ch.utils
  *
@@ -8,16 +10,28 @@ package com.ch.utils;
  * @version 1.0
  * @since 1.8
  */
-public class StringExtUtils  {
+public class StringExtUtils {
 
     private StringExtUtils() {
     }
 
+    /**
+     *
+     * @param number
+     * @param length
+     * @return
+     */
     public static String formatSort(Number number, int length) {
         if (number == null) number = 0;
         return String.format("%0" + length + "d", number);
     }
 
+    /**
+     *
+     * @param s
+     * @param separator
+     * @return
+     */
     public static String lastStr(String s, String separator) {
         if (CommonUtils.isEmpty(s)) return "";
         if (s.contains(separator)) {
@@ -25,5 +39,29 @@ public class StringExtUtils  {
             return s.substring(i);
         }
         return s;
+    }
+
+    /**
+     * 链接字符串数组
+     *
+     * @param separator 连接符号
+     * @param args 符串数组
+     * @return
+     */
+    public static String linkStr(String separator, String... args) {
+        if (CommonUtils.isEmpty(args)) return "";
+        if (CommonUtils.isEmpty(separator)) separator = Constants.SEPARATOR;
+        StringBuilder sb = new StringBuilder();
+        for (String str : args) {
+            if (CommonUtils.isEmpty(str)) {
+                continue;
+            }
+            if (sb.length() > 0) {
+                sb.append(separator).append(str);
+            } else {
+                sb.append(str);
+            }
+        }
+        return sb.toString();
     }
 }
