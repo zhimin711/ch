9e0a603a68d5ea536ce14f852e6b7c261de0a030
仙_梦如尘 <zhimin711@sina.com>
2019-07-04 17:28:23 +0800

    拆分常量状态

---
+++ ch-core/src/main/java/com/ch/NumS.java
@@ -0,0 +1,28 @@
+package com.ch;
+
+/**
+ * 描述：com.zh
+ *
+ * @author zhimin.ma
+ * 2017/2/4.
+ * @version 1.0
+ * @since 1.8
+ */
+public class NumS {
+
+    /**
+     * String type 0~9
+     */
+    public final static String _0 = "0";
+    public final static String _1 = "1";
+    public final static String _2 = "2";
+    public final static String _3 = "3";
+    public final static String _4 = "4";
+    public final static String _5 = "5";
+    public final static String _6 = "6";
+    public final static String _7 = "7";
+    public final static String _8 = "8";
+    public final static String _9 = "9";
+
+
+}
--- ch-core/src/main/java/com/ch/StatusS.java
+++ ch-core/src/main/java/com/ch/StatusS.java
@@ -3,18 +3,18 @@ package com.ch;
 /**
  * 描述：com.zh
  *
- * @author 80002023
+ * @author zhimin.ma
  * 2017/2/4.
  * @version 1.0
  * @since 1.8
  */
-public class StatusStr {
+public class StatusS {
 
     /**
      *
      */
-    public final static String UNKNOWN = "-1", NULL = "-1";
-    public final static String FAILED = "0", DISABLED = "0", HALTED = "0";
+    public final static String UNKNOWN = "-1", NULL = "-1", END = "-1";
+    public final static String FAILED = "0", DISABLED = "0", UNSELECTED = "0", HALTED = "0", BEGIN = "0";
 
     public final static String SUCCESS = "1", ENABLED = "1", SELECTED = "1", LEAF = "1", RUNNING = "1", SYS = "1";
     public final static String ERROR = "2", PAUSED = "2";
