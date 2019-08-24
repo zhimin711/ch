68195215ff558fe89a241f76dd2adc76c350e205
仙_梦如尘 <zhimin711@sina.com>
2019-08-24 09:14:10 +0800

    调整 table service

--- ch-mybatis/src/main/java/com/ch/mybatis/mapper/TableMapper.java
+++ ch-mybatis/src/main/java/com/ch/mybatis/mapper/TableMapper.java
@@ -5,10 +5,27 @@ import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface TableMapper {
+    /**
+     * 复制表结构
+     *
+     * @param sourceTableName 源表名
+     * @param targetTableName 新表名
+     */
+    void create(@Param("sourceName") String sourceTableName, @Param("targetName") String targetTableName);
 
-    void create(@Param("sourceName") String sourceName, @Param("targetName") String targetName);
-
+    /**
+     * 截断表
+     *
+     * @param tableName 表名
+     */
     void truncate(@Param("tableName") String tableName);
 
+    /**
+     * 指定数据库检查表是否存在
+     *
+     * @param schemaName 库名
+     * @param tableName 表名
+     * @return 0/1
+     */
     int isExists(@Param("schemaName") String schemaName, @Param("tableName") String tableName);
 }
