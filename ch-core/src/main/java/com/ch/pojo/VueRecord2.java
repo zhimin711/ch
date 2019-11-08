b504cdcfb53e278cfeb7758506428995fdcb026d
仙_梦如尘 <zhimin711@sina.com>
2019-11-08 10:16:13 +0800

    vue

---
+++ ch-core/src/main/java/com/ch/pojo/VueRecord2.java
@@ -0,0 +1,19 @@
+package com.ch.pojo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author zhimin
+ * @date 2018/12/29 10:54
+ */
+@Data
+public class VueRecord2 extends VueRecord {
+
+    /**
+     * 关键字
+     */
+    private String key;
+}
