27a1b8ede30dc2679aa04a8071d8f7591ed3e84d
仙_梦如尘 <zhimin711@sina.com>
2019-11-14 14:48:04 +0800

    ex utils

--- ch-mybatis/src/main/java/com/ch/mybatis/utils/ExampleUtils.java
+++ ch-mybatis/src/main/java/com/ch/mybatis/utils/ExampleUtils.java
@@ -5,6 +5,7 @@ import com.ch.utils.BeanExtUtils;
 import com.ch.utils.CommonUtils;
 import com.ch.utils.SQLUtils;
 import tk.mybatis.mapper.entity.Example;
+import tk.mybatis.mapper.util.Sqls;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -210,5 +211,182 @@ public class ExampleUtils {
          * 前模糊
          */
         LIKE_PREFIX
+    }
+
+
+
+    /**
+     * @param sqls
+     * @param property
+     * @param values
+     * @return
+     */
+    public static int dynIn(Sqls sqls, String property, List<?> values) {
+
+        if (CommonUtils.isEmpty(values)) {
+            return 0;
+        } else {
+            sqls.andIn(property, values);
+        }
+        return 1;
+    }
+
+    /**
+     * 动态拼接指定字段条件
+     *
+     * @param sqls
+     * @param property
+     * @param values
+     * @return
+     */
+    public static int dynInOrLike(Sqls sqls, String property, List<String> values) {
+        if (CommonUtils.isEmpty(values)) {
+            return 0;
+        } else if (values.size() == 1) {
+            sqls.andLike(property, "%" + values.get(0) + "%");
+        } else {
+            sqls.andIn(property, values);
+        }
+        return 1;
+    }
+
+    /**
+     * 动态拼接基础字段条件
+     *
+     * @param sqls 条件装配
+     * @param record   条件
+     * @return 装配条件数
+     */
+    public static int dynCommon(Sqls sqls, Object record) {
+        AtomicInteger c = new AtomicInteger(0);
+        if (record == null) return c.get();
+        c.getAndAdd(dynEqual(sqls, record, BASE));
+        c.getAndAdd(dynLike(sqls, record, LIKE));
+        return c.get();
+    }
+
+    /**
+     * 动态拼接指定字段条件(equal or in)
+     *
+     * @param sqls   条件装配
+     * @param record     条件
+     * @param properties 属性
+     * @return 装配条件数
+     */
+    public static int dynEqual(Sqls sqls, Object record, String... properties) {
+        AtomicInteger c = new AtomicInteger(0);
+        if (record == null) return c.get();
+        for (String r : properties) {
+            Object v = BeanExtUtils.getValueByProperty(record, r);
+            if (CommonUtils.isEmpty(v)) {
+                continue;
+            }
+            if (v instanceof Iterable) {
+                sqls.andIn(r, (Iterable) v);
+            } else {
+                sqls.andEqualTo(r, v);
+            }
+            c.addAndGet(1);
+        }
+        return c.get();
+    }
+
+    /**
+     * 动态拼接指定字段条件(equal or in)
+     *
+     * @param sqls   条件装配
+     * @param record     条件
+     * @param properties 属性
+     * @return 装配条件数
+     */
+    public static int dynNotEqual(Sqls sqls, Object record, String... properties) {
+        AtomicInteger c = new AtomicInteger(0);
+        if (record == null) return c.get();
+        for (String r : properties) {
+            Object v = BeanExtUtils.getValueByProperty(record, r);
+            if (CommonUtils.isEmpty(v)) {
+                continue;
+            }
+            sqls.andNotEqualTo(r, v);
+            c.addAndGet(1);
+        }
+        return c.get();
+    }
+
+    /**
+     * 动态拼接指定字段条件(equal or in)
+     *
+     * @param sqls      条件装配
+     * @param record        条件
+     * @param conditionType 条件类型
+     * @param property      属性
+     * @return 装配条件数
+     */
+    public static int dynType(Sqls sqls, Object record, ConditionType conditionType, String property) {
+        AtomicInteger c = new AtomicInteger(0);
+        if (record == null) return c.get();
+        if (conditionType == ConditionType.NOT_NULL) {
+            sqls.andIsNotNull(property);
+            return 1;
+        } else if (conditionType == ConditionType.NULL) {
+            sqls.andIsNull(property);
+            return 1;
+        }
+        Object v = BeanExtUtils.getValueByProperty(record, property);
+        if (v == null) {
+            return 0;
+        }
+        if (conditionType == ConditionType.NOT_EQUAL_TO) {
+            sqls.andNotEqualTo(property, v);
+        } else if (conditionType == ConditionType.GREATER_THAN) {
+            sqls.andGreaterThan(property, v);
+        } else if (conditionType == ConditionType.LESS_THAN) {
+            sqls.andLessThan(property, v);
+        } else {
+            sqls.andEqualTo(property, v);
+        }
+        c.addAndGet(1);
+        return c.get();
+    }
+
+    /**
+     * 动态拼接指定字段Like条件（只装配String字符串）
+     *
+     * @param sqls   条件装配
+     * @param record     条件
+     * @param properties 属性
+     * @return 装配条件数
+     */
+    public static int dynLike(Sqls sqls, Object record, String... properties) {
+        return dynLike(sqls, record, LikeType.LIKE_ANY, properties);
+    }
+
+    /**
+     * 动态拼接指定字段Like条件（只装配String字符串）
+     *
+     * @param sqls   条件装配
+     * @param record     条件
+     * @param likeType   模糊类型
+     * @param properties 属性
+     * @return 装配条件数
+     */
+    public static int dynLike(Sqls sqls, Object record, LikeType likeType, String... properties) {
+        AtomicInteger c = new AtomicInteger(0);
+        if (record == null) return c.get();
+        for (String r : properties) {
+            Object v = BeanExtUtils.getValueByProperty(record, r);
+            if (CommonUtils.isEmpty(v) || !(v instanceof String)) continue;
+            String tmp = ((String) v).trim();
+            if (likeType == LikeType.LIKE_ANY) {
+                tmp = SQLUtils.likeAny(tmp);
+            } else if (likeType == LikeType.LIKE_SUFFIX) {
+                tmp = SQLUtils.likeSuffix(tmp);
+            } else if (likeType == LikeType.LIKE_PREFIX) {
+                tmp = SQLUtils.likePrefix(tmp);
+            }
+            sqls.andLike(r, tmp);
+            c.addAndGet(1);
+        }
+        return c.get();
     }
 }
