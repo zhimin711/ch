b641442b2605e4fde76a058f4d48878a6413d72a
仙_梦如尘 <zhimin711@sina.com>
2020-06-26 15:41:24 +0800

    域名解析调整

--- ch-core/src/main/java/com/ch/utils/NetUtils.java
+++ ch-core/src/main/java/com/ch/utils/NetUtils.java
@@ -29,11 +29,11 @@ public class NetUtils {
      * 判断IP格式和范围
      */
     public final static String REGEX_IP = "([1-9]|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3}";
-    public final static String REGEX_IP_PROTOCOL = "^((http|https)://)?([1-9]|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3}?(/)";
+    public final static String REGEX_IP_PROTOCOL = "^((http|https)://)?([1-9]|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3}";
     /**
      * 域名正则表达式1
      */
-    public final static String REGEX_DOMAIN = "^((http://)|(https://))?([a-zA-Z0-9]([a-zA-Z0-9\\-]{0,61}[a-zA-Z0-9])?\\.)+[a-zA-Z]{2,6}(/)";
+    public final static String REGEX_DOMAIN = "^((http://)|(https://))?([a-zA-Z0-9]([a-zA-Z0-9\\-]{0,61}[a-zA-Z0-9])?\\.)+[a-zA-Z]{2,6}";
     /**
      * 域名正则表达式2
      */
